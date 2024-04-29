0e17ca6359d089708a836b1c1ac7a7c35a4383d3
GBRRRL <97097069+gbrrrl@users.noreply.github.com>
2024-04-29 13:42:51 -0400

    Create disease-theme.js

---
+++ disease-theme.js
@@ -0,0 +1,54 @@
+var body = $("body"),
+  grid = $("#posts"),
+  article = $(".entry");
+article.fitVids(),
+  article.each(article_setup),
+  body.is("#I")
+    ? (grid
+        .css("min-height", $(window).height() + 80 + "px")
+        .imagesLoaded(function () {
+          photoset(),
+            setTimeout(function () {
+              grid.masonry({
+                itemSelector: ".entry",
+                columnWidth: 1,
+                isFitWidth: !0,
+              }),
+                body.css("opacity", "1"),
+                article.addClass("loaded");
+            }, 10),
+            sidebar.delay(1e3).queue(function () {
+              $(this).addClass("loaded");
+            });
+        }),
+      grid.infinitescroll(
+        {
+          navSelector: "#pagination",
+          nextSelector: "#pagination a#older",
+          itemSelector: ".entry",
+          bufferPx: 500,
+        },
+        function (t) {
+          var i = $(t).addClass("loading"),
+            e = i
+              .map(function () {
+                return this.id;
+              })
+              .get();
+          Tumblr.LikeButton.get_status_by_post_ids(e),
+            i.imagesLoaded(function () {
+              i.each(article_setup),
+                photoset(),
+                setTimeout(function () {
+                  grid.masonry("appended", i, !0),
+                    i.removeClass("loading").addClass("loaded");
+                }, 10);
+            });
+        }
+      ))
+    : grid.imagesLoaded(function () {
+        body.css("opacity", "1"),
+          sidebar.delay(1e3).queue(function () {
+            $(this).addClass("loaded");
+          });
+      });
